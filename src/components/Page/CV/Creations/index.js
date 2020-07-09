5aebf3d41f3dffa510c3e91dfaadc307c570009e
Marine Berthier <marine154@hotmail.fr>
2020-07-09 16:18:12 +0200

    fix bug images

--- src/components/Page/CV/Creations/index.js
+++ src/components/Page/CV/Creations/index.js
@@ -7,6 +7,12 @@ import Fade from 'react-reveal/Fade';
 // == Import : local
 import './creations.scss';
 
+import imageDark from './dark.png';
+import imagePale from './pale.png';
+import imagePaper from './paper.jpg';
+import imageSite from './site-CL.png';
+import imageTape from './tape.png';
+
 // == Composant
 const Creations = ({ language, content, mode }) => (
   <div id="creations">
@@ -16,14 +22,14 @@ const Creations = ({ language, content, mode }) => (
       <Fade top>
         <div id="website-list">
           <div className="website-item">
-            <a className="website-link" href="https://www.laverriere-christian.com/"><img src="src/components/Page/CV/Creations/site-CL.png" alt="screenshot website" />
+            <a className="website-link" href="https://www.laverriere-christian.com/"><img src={imageSite} alt="screenshot website" />
 
               {language === 'fr' ? <p>Site Vitrine (React) pour un artisan <GiSpade size="1.5em" /></p> : <p>Website (React) for a small business <GiSpade size="1.5em" /></p>}
             </a>
           </div>
 
           <div className="website-item">
-            {mode === true ? <img className="img-dot" src="src/components/Page/CV/Creations/dark.png" alt="dot dot dot" /> : <img className="img-dot" src="src/components/Page/CV/Creations/pale.png" alt="dot dot dot" />}
+            {mode === true ? <img className="img-dot" src={imageDark} alt="dot dot dot" /> : <img className="img-dot" src={imagePale} alt="dot dot dot" />}
 
             {language === 'fr' ? <p>Du nouveau très bientôt <GiHourglass size="1.5em" /></p> : <p>More to come very soon <GiHourglass size="1.5em" /></p>}
 
@@ -34,7 +40,7 @@ const Creations = ({ language, content, mode }) => (
 
         {language === 'fr' ? content.fr.content : content.ang.content}  <a href="https://github.com/Marine-Berthier"><div className="icon"> <FaGithub className="git" size="2.6em" /> </div> </a>
       </div>
-      <img className="tape" src="src/components/Page/CV/Creations/tape.png" />
+      <img className="tape" src={imageTape} />
     </div>
   </div>
 );
